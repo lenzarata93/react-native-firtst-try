50576213f9bd21eb18725747abef42796b6f9967
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2024-05-18 14:24:30 +0300

    adding android package

--- react-native-first-try/App.js
+++ react-native-first-try/App.js
@@ -1,37 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 
 export default function App() {
-  console.log("ITS WORKING");
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Name</Text>
-      <TextInput
-        style={styles.input}
-        value={name}
-        onChangeText={setName}
-      />
+    <SafeAreaView style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require("./assets/изтеглен файл.jpg")}></Image>
 
-      <Text style={styles.label}>Email</Text>
-      <TextInput
-        style={styles.input}
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-
-      <Text style={styles.label}>Password</Text>
-      <TextInput
-        style={styles.input}
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-
-      <Button title="Register" onPress={handleRegister} />
-    </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
+}
+const style = {
+  backgroundColor: "red",
+  textAlign: "center",
 };
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "140px",
+  },
+});
