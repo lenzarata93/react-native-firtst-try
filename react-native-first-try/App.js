3fa127c736894eaea29f112843ff8c6a56f72f75
lenzarata93 <117170826+lenzarata93@users.noreply.github.com>
2024-05-18 14:46:09 +0300

    Update App.js
    adding button wiht some style

--- react-native-first-try/App.js
+++ react-native-first-try/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
       <Image source={require("./assets/изтеглен файл.jpg")}></Image>
-
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.textBtn}>CLICK ME</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -21,6 +33,20 @@ const styles = StyleSheet.create({
     backgroundColor: "blue",
     justifyContent: "center",
     alignItems: "center",
-    fontSize: "140px",
+  },
+  text: {
+    fontSize: 20,
+    color: "white",
+  },
+  btn: {
+    borderRadius: 10,
+    borderColor: "silver",
+    borderWidth: 2,
+    padding: 10,
+    marginTop: 15,
+  },
+  textBtn: {
+    fontSize: 16,
+    color: "silver",
   },
 });
